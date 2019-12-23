2cd9e535931dbd458b1eb799794d6ba403733417
Abishek <abisubramanya27@gmail.com>
2019-12-23 17:26:16 +0530

    changed prc.join()

--- app/application.py
+++ app/application.py
@@ -4,14 +4,14 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length, EqualTo
 from flask_session import Session
 import os
-from app.class_orm import db,User,Result,Submission
+from class_orm import db,User,Result,Submission
 import time
 from datetime import datetime, timedelta
 from werkzeug import generate_password_hash,check_password_hash
 import threading
 import re
 import sys
-from app.qnEvaluate import score
+from qnEvaluate import score
 from flask_socketio import SocketIO, emit
 import decimal 
 from sqlalchemy import nullslast
@@ -25,7 +25,7 @@ app.secret_key = 'Thisisnottobesharedtoanyone'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=2)
 
-ENV = 'PROD'
+ENV = 'dev'
 if ENV == 'dev' :
 	app.debug = True
 	app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
--- app/qnEvaluate.py
+++ app/qnEvaluate.py
@@ -2,17 +2,17 @@ import filecmp
 import os
 import re
 import multiprocessing
-from app.compiler import interpret
+from compiler import interpret
 
 noTC = {'1': 1,'2': 19,'3': 19,'4': 19,'5': 16}
 
 def score(code,qn_no,pno) :
 	count = 0
-	inputPath = './app/evaluation/input/qn'+qn_no
+	inputPath = './evaluation/input/qn'+qn_no
 	for filename in os.listdir(inputPath) :
 		if 'tc' in filename :
 			fno = re.sub('[^0-9]+','',filename)
-			outputfilePath = './app/evaluation/output' + pno + '.txt'
+			outputfilePath = './evaluation/output' + pno + '.txt'
 			with open(outputfilePath,'w+') as mfile :
 				count += 1
 				inpfilePath = inputPath + '/' + filename
@@ -25,14 +25,15 @@ def score(code,qn_no,pno) :
 
 				if prc.is_alive() :
 					prc.terminate()
+					prc.join(1)
 					mfile.close()
 					os.remove(outputfilePath)
 					return 'TIME LIMIT EXCEEDED'
 				else :
 					Message = Q.get()
 					if Message == 'ANSWER WRITTEN' :
-						with open('./app/evaluation/expected_output/qn'+qn_no+'/output-'+str(fno)+'.txt') as tgtfile :
-							if filecmp.cmp(outputfilePath,'./app/evaluation/expected_output/qn'+qn_no+'/output-'+str(fno)+'.txt') :
+						with open('./evaluation/expected_output/qn'+qn_no+'/output-'+str(fno)+'.txt') as tgtfile :
+							if filecmp.cmp(outputfilePath,'./evaluation/expected_output/qn'+qn_no+'/output-'+str(fno)+'.txt') :
 								pass
 							else :
 								tgtfile.close()
